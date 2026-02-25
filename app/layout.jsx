2e8e69575138c994690883a96ebadb80c3473d1d
Christos <ctsoutsouras@gmail>
2026-02-25 23:02:13 +0200

    disable static export

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const dynamic = "force-dynamic";
+
+export const metadata = {
+  title: "BLOSS Verification",
+  description: "BLOSS Authenticity Check",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="el">
+      <body>{children}</body>
+    </html>
+  );
+}
