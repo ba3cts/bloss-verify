8b78654af6c57c21e36bfd964129c4bf9bb15d4e
ba3cts <ctsoutsouras@gmail.com>
2026-02-26 01:40:28 +0200

    Update supabase.js

--- app/app/lib/supabase.js
+++ app/app/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ejflpanpfifrjfppdswb.supabase.co";
-const supabaseAnonKey = "ΒΑΛΕ_ΤΟ_PUBLISHABLE_KEY_ΣΟΥ";
+const supabaseAnonKey = "sb_publishable_IVIIyBQjuynZLNgGfROH-Q_msgGjYwz";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
