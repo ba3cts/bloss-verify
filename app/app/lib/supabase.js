e3aede373b0d108ad99f6d212ee499e55bec757f
ba3cts <ctsoutsouras@gmail.com>
2026-02-26 01:49:25 +0200

    Delete app/app directory

--- app/app/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://ejflpanpfifrjfppdswb.supabase.co";
-const supabaseAnonKey = "sb_publishable_IVIIyBQjuynZLNgGfROH-Q_msgGjYwz";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
